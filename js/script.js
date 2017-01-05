a096cccda7a04f10b54095f60f501e1ef5ea57fa
mjhui1990 <mjhui90@gmail.com>
2017-01-04 21:07:29 -0500

    updated js file

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@ var sumOfNumbers = 0
 
 for (var i = 0; i < 1000; i++) {
 
-    if((i % 3 || i % 5) === 0) {
-        (i + sumOfNumbers);
+    if((i % 3 === 0 || i % 5 === 0) ) {
+        (sumOfNumbers+=i);
     }
 }
 
